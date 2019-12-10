734600dad505c07f249d5b853953b9dbdf90fb8f
Martha Caraveo <martha.caraveo@uabc.edu.mx>
2019-12-10 00:48:17 -0800

    Soy el mapilla

--- proyecto/.ipynb_checkpoints/models-checkpoint.py
+++ proyecto/.ipynb_checkpoints/models-checkpoint.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Tabla_test(models.Model):
-    columna_uno = models.CharField(max_length=200)
-    columna_dos = models.CharField(max_length=200)
-    columna_tres = models.CharField(max_length=200)
-    columna_cuatro = models.CharField(max_length=200)
+class user_model(models.Model):
+    ##id = models.CharField(max_length=200)
+    born_date = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200)
+        
+        
+class position_model(models.Model):
+   ## id=models.CharField(max_length=200)
+    altitude=models.CharField(max_length=200)
+    latitude=models.CharField(max_length=200)
+    longitude=models.CharField(max_length=200)
+    user_id=models.CharField(max_length=200)
